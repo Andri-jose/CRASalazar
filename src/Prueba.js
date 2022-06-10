8b50ac3038c340d8457131f67ae0dfc8a47a13c6
Andri Salazar <andr-y2008@hotmail.com>
2022-06-10 20:40:35 -0300

    First Commit

---
+++ src/Prueba.js
@@ -0,0 +1,12 @@
+function myApp(props){
+    return (
+        <div className="prueba">
+            <p>
+              example text Hello {props.nombre}
+            </p>
+        </div>
+      );
+}
+
+
+export default myApp;
